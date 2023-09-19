27bcd277855d34095ec5c8f72e7a54e5b966dc6f
Ned Palacios <npdoesmc@gmail.com>
2023-09-19 20:03:06 +0800

    fix wrong linkedin param for org id

--- src/pages/CertificateScreen.vue
+++ src/pages/CertificateScreen.vue
@@ -42,12 +42,13 @@ const certImageUrl = ref('');
 const certificateImgCss = computed(() => `background-image: url(${certImageUrl.value});`);
 const certId = computed(() => `DSSS2023-${authState.participantInfo.docId}`);
 const linkedInShareUrl = computed(() => {
-  const orgName = '96606526';
+  // const orgName = 'DICE - Davao Interschool Computer Enthusiasts';
+  const orgId = '96606526';
   const certTitle = '1st Davao Student Startup Summit 2023: Sparking Insights and Ideas for Your Next Innovation';
   const certYear = 2023;
   const certMonth = 9;
   const certURL = window.location.origin + `/verify/${certId.value}`;
-  return `https://www.linkedin.com/profile/add?startTask=CERTIFICATION_NAME&organizationName=${encodeURI(orgName)}&name=${certTitle}&issueYear=${certYear}&issueMonth=${certMonth}&certUrl=${certURL}&certId=${certId.value}`;
+  return `https://www.linkedin.com/profile/add?startTask=CERTIFICATION_NAME&organizationId=${encodeURI(orgId)}&name=${certTitle}&issueYear=${certYear}&issueMonth=${certMonth}&certUrl=${certURL}&certId=${certId.value}`;
 });
 
 if (route.name === 'cert-with-id-page' && !authState.participantInfo.fromVerify) {
